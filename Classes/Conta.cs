5b2c100a7b69b0719c2d231cb3964aac1a1a2d9c
Ricardo Silva <rigsjf@gmail.com>
2021-10-29 13:27:40 -0300

    first comit

---
+++ Classes/Conta.cs
@@ -0,0 +1,33 @@
+
+using System;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set;}
+        private double Saldo { get; set;}
+        private double Credito { get; set;}
+        private string Nome { get; set;}
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            //Validação de saldo suficiente
+            if(this.Saldo - valorSaque < (this.Credito *-1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+    }
+}
